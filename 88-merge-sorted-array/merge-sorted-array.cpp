3896abe5b40ab69c6ebbffa9a47932588a77ddb5
Lakshay-Dahiya <112479095+Lakshay-Dahiya@users.noreply.github.com>
2025-05-06 13:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (95.67%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,9 +1,44 @@
 class Solution {
+    private:
+    void swapIfGreater(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2){
+        if(nums1[ind1] > nums2[ind2]){
+            swap(nums1[ind1], nums2[ind2]);
+        }
+    }
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i<n; i++){ //  OP approach
-            nums1[m+i] = nums2[i];
+        int len = m+n;
+        int gap = (len/2) + (len%2);
+        while(gap>0){
+            int left = 0;
+            int right = left + gap;
+            while(right < len){
+                // swap elements of arr1 and arr2
+                if(left < m && right >= m){
+                    swapIfGreater(nums1,nums2, left, right-m);
+                }
+                // swap elements of arr2 and arr2
+                // right - m to access correct index of nums2
+
+                else if(left >= m){
+                    swapIfGreater(nums2,nums2, left-m, right-m);
+                }
+
+                // swap elements of arr1 and arr1
+                else {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+                //else{
+                    // skip if both in nums2 → no nee
+                //}
+                left++, right++;
+            }
+            if(gap==1) break;
+            gap = (gap/2) + (gap%2);
         }
-        sort(nums1.begin(), nums1.end());
+        //Copy nums2 into nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
     }
 };
