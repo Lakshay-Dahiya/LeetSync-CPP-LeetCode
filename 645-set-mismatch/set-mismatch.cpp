d2f105d171db2ca4a6850272bab60fb1839d396d
Lakshay-Dahiya <112479095+Lakshay-Dahiya@users.noreply.github.com>
2025-05-07 23:22:52 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (86.38%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        //int hash[n+1] = {0};
-        vector<int> hash(n + 1, 0);
-        for(int i=0; i<n; i++){
-            hash[nums[i]]++;
-        }
-        int repeating = -1, missing = -1;
-        for(int i=1; i<=n; i++){
-            if(hash[i] == 2) repeating = i;
-            else if(hash[i] == 0) missing = i;
+        long long n = nums.size();
+        long long s=0, sn=0;
+        long long s2=0, s2n=0;
+        sn = (n * (n+1)) / 2;
+        s2n = (n * (n+1) * (2*n+1)) / 6;
 
-            if(repeating != -1 && missing != -1){
-                break;
-            }
+        for(int i=0; i<n; i++){
+            s += nums[i];
+            s2 += (long long)nums[i] * (long long)nums[i];
         }
-        return {repeating,missing};
+
+        long long val1 = s - sn; // x-y
+        long long val2 = (s2 - s2n) / val1; //x+y
+        long long x = (val1 + val2) / 2;
+        long long y = x - val1;
+
+        return {(int)x,(int)y};
 
     }
 };
