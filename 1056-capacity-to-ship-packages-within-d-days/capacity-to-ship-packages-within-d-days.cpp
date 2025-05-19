6a60fc7384d4e24e87e5fdd2504b34dbf3d4168c
Lakshay-Dahiya <112479095+Lakshay-Dahiya@users.noreply.github.com>
2025-05-20 02:33:01 +0530

    Time: 14 ms (40.17%) | Memory: 34.8 MB (94.31%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -16,15 +16,12 @@ public:
         return days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        
         int low = *max_element(weights.begin(), weights.end());
         int high = accumulate(weights.begin(), weights.end(), 0);
         while(low <= high){
             int mid = (low + high)/2;
             int numberOfDays = findDays(weights, mid);
-            if(numberOfDays <= days){
-                high = mid - 1;
-            }
+            if(numberOfDays <= days) high = mid - 1;
             else low = mid + 1;
         }
         return low;
